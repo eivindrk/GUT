3745a6fb0a99a9fa644d3ffa5d2ee94f53f3f253
Bror Eivind <karlsen.r.eivind@gmail.com>
2025-03-06 00:19:53 +0100

    Front page with dropdown menu fixed

---
+++ someJava.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    function(){
+        $(".hamburger-icon").on("click", function(){
+            // $(".dropdown-menu").toggleClass("open").stop(true, false).slideToggle(300);
+            $(".dropdown-menu").slideToggle(300);
+        });
+
+        $(".close-button").on("click", function(){
+            $(".dropdown-menu").slideToggle(300);
+        })
+});
