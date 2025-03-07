f5fa54289c86c75c17957f330c61de45eca3f975
Bror Eivind <karlsen.r.eivind@gmail.com>
2025-03-07 11:13:08 +0100

    Grids for bio adjusted

--- someJava.js
+++ someJava.js
@@ -3,9 +3,16 @@ $(document).ready(
         $(".hamburger-icon").on("click", function(){
             // $(".dropdown-menu").toggleClass("open").stop(true, false).slideToggle(300);
             $(".dropdown-menu").slideToggle(300);
+            $(".single-container").slideToggle(300);
+            $(".social-media-container").slideToggle(300);
+
         });
 
         $(".close-button").on("click", function(){
             $(".dropdown-menu").slideToggle(300);
+            $(".single-container").slideToggle(300);
+            $(".social-media-container").slideToggle(300);
         })
+
+        
 });
